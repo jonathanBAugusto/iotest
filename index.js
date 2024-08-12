400336159ffdecbeb6389a5c12e58ca67c7eb0cf
Jhon Doe Dev <jonathanbersot21@gmail.com>
2024-08-12 15:37:43 -0300

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const animationPath = '/assets/favicon_animation_frames'
+
+const animationLoop = (frames, intervalMS) => {
+    let counter = 0;
+    setInterval(() => {
+        const favicon = document.getElementById('favico');
+        favicon.setAttribute('href', `${animationPath}/frame_${counter}.png`);
+        counter < frames ? counter++ : counter= 0;
+    }, intervalMS);
+}
+
+animationLoop(36, 200);
